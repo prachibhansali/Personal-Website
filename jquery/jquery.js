72928911fa6651afa74eb34c13f452fabd887623
Prachi Bhansali <prachibh@ccs.neu.edu>
2015-08-02 20:42:34 -0400

    Added header background and bouncing button

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
-	$('#scrollUp').click(function(){
-		$('.header').slideUp(1000);
+
+	$('#scrollUp').css('left',$('.header').width()/2 - $('#scrollUp').width()/2);
+	
+
+	/*var animateMe = function(targetElement){
+		$(this).css('top',19);
+		$(this).animate({top: '20%'},{duration: 400,complete: function(){
+			animateMe(this);}
+		});
+	};
+
+	animateMe('#scrollUp');*/
+
+	$('#scrollUp').one('click',function(){
+		var resumeButton="<div class=\"buttonCenter\"> <button>Resume</button> <button>Contact</button> </div>";	
+		$('.header').animate({bottom: '50%'});//height: $('.header').height()/2},1000);
+		$('.mainPage').show();
+		$('#headerReplacer').replaceWith(resumeButton);
+		$('#headerReplacer').css('left','+=100');
+
 	});
 });
