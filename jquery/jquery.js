c0417c42b063982b9ef8b0ac2be7edf6cb5534d5
Prachi Bhansali <prachibh@ccs.neu.edu>
2015-08-02 23:55:05 -0400

    Modified background to black and added button animation to switch color

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 
 	$('#scrollUp').css('left',$('.header').width()/2 - $('#scrollUp').width()/2);
-	
-
 	/*var animateMe = function(targetElement){
 		$(this).css('top',19);
 		$(this).animate({top: '20%'},{duration: 400,complete: function(){
@@ -12,12 +10,33 @@ $(document).ready(function() {
 
 	animateMe('#scrollUp');*/
 
-	$('#scrollUp').one('click',function(){
-		var resumeButton="<div class=\"buttonCenter\"> <button>Resume</button> <button>Contact</button> </div>";	
-		$('.header').animate({bottom: '50%'});//height: $('.header').height()/2},1000);
-		$('.mainPage').show();
-		$('#headerReplacer').replaceWith(resumeButton);
-		$('#headerReplacer').css('left','+=100');
+	
 
+	$('#resumelink').hover(function(){
+		$(this).addClass("switchLinkColor");
+	}, function() {
+		$(this).removeClass("switchLinkColor");
 	});
+
+	$('#contactlink').hover(function(){
+		$(this).addClass("switchLinkColor");
+	}, function() {
+		$(this).removeClass("switchLinkColor");
+	});
+
+	$('#scrollUp').one('click',function(){
+		/*var resumecontact = "<div id=\"resumecontact\" class=\"buttonCenter\">" +  
+						"<a href=\"#\" id=\"resumelink\" class=\"resumecontactlink\">Resume</a>"+
+						"<div class=\"divider\"/>"+ 
+						"<a href=\"#\" id=\"contactlink\" class=\"resumecontactlink\">Contact</button>"+ 
+						"</div>";*/
+		$('.header').animate({bottom: '60%'});//height: $('.header').height()/2},1000);
+		$('.mainPage').show();
+		$('#scrollUp').hide();
+		$('#resumecontact').show();
+		$('#headerReplacer').css('left','+=100');
+		$('#resumecontact').css('left',$('.header').width()/2 - $('#resumecontact').width()/2);
+		
+	});
+
 });
